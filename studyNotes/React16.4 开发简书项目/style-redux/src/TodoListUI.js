8dc7c7cc2b90e6a3cf58e48ad6542c5f1126ca20
CoolPet <191261005@qq.com>
2019-12-04 16:53:50 +0800

    React16.4 开发简书项目

--- studyNotes/React16.4 开发简书项目/style-redux/src/TodoListUI.js
+++ studyNotes/React16.4 开发简书项目/style-redux/src/TodoListUI.js
@@ -3,32 +3,60 @@ import { Input, Button, List } from "antd"
 
 const Fragment = React.Fragment
 
-class TodoListUI extends Component{
-  render(){
-    const { inputValue, list, handleInputChange, submit, deleteItem } = this.props
-    return (
-      <Fragment>
-        <div className="header">
-          <Input 
-            value={inputValue} 
-            placeholder="todo info" 
-            className="int"
-            onChange={handleInputChange}/>
-          <Button type="primary" onClick={submit}>提交</Button>
-        </div>
-        <List
-          className="list"
-          bordered
-          dataSource={list}
-          renderItem={(item, index) => (
-            <List.Item onClick={() => {deleteItem(index)}}>
-              {item}
-            </List.Item>
-          )}
-        />
-      </Fragment>
-    )
-  }
+// 无状态组件
+const TodoListUI = (props) => {
+  const { inputValue, list, handleInputChange, submit, deleteItem } = props
+  return (
+    <Fragment>
+      <div className="header">
+        <Input 
+          value={inputValue} 
+          placeholder="todo info" 
+          className="int"
+          onChange={handleInputChange}/>
+        <Button type="primary" onClick={submit}>提交</Button>
+      </div>
+      <List
+        className="list"
+        bordered
+        dataSource={list}
+        renderItem={(item, index) => (
+          <List.Item onClick={() => {deleteItem(index)}}>
+            {item}
+          </List.Item>
+        )}
+      />
+    </Fragment>
+  )
 }
+
+// UI 分组件和容器组件分离
+// class TodoListUI extends Component{
+//   render(){
+//     const { inputValue, list, handleInputChange, submit, deleteItem } = this.props
+//     return (
+//       <Fragment>
+//         <div className="header">
+//           <Input 
+//             value={inputValue} 
+//             placeholder="todo info" 
+//             className="int"
+//             onChange={handleInputChange}/>
+//           <Button type="primary" onClick={submit}>提交</Button>
+//         </div>
+//         <List
+//           className="list"
+//           bordered
+//           dataSource={list}
+//           renderItem={(item, index) => (
+//             <List.Item onClick={() => {deleteItem(index)}}>
+//               {item}
+//             </List.Item>
+//           )}
+//         />
+//       </Fragment>
+//     )
+//   }
+// }
 
 export default TodoListUI
