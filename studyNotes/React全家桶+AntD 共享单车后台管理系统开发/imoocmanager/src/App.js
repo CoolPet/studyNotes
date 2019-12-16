8859f64d3dcf4a74158fd0b8128c6622ac895a5f
CoolPet <191261005@qq.com>
2019-12-16 14:44:40 +0800

    React全家桶+AntD 共享单车后台管理系统开发

--- studyNotes/React全家桶+AntD 共享单车后台管理系统开发/imoocmanager/src/App.js
+++ studyNotes/React全家桶+AntD 共享单车后台管理系统开发/imoocmanager/src/App.js
@@ -1,8 +1,13 @@
 import React from "react"
+import { Button } from "antd"
 
 class App extends React.Component{
   render(){
-    return <div>App</div>
+    return (
+      <div>
+        <Button type="primary">Primary</Button>
+      </div>
+    )
   }
 }
 
