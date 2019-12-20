f971a6b7d351886b2b29f505ca627e72fc9efd4d
CoolPet <191261005@qq.com>
2019-12-20 17:30:40 +0800

    React全家桶+AntD 共享单车后台管理系统开发

--- studyNotes/React全家桶+AntD 共享单车后台管理系统开发/imoocmanager/src/axios/index.js
+++ studyNotes/React全家桶+AntD 共享单车后台管理系统开发/imoocmanager/src/axios/index.js
@@ -1,15 +1,28 @@
-import JsonP from "jsonp"
+import axios from "axios"
+import { Modal } from "antd"
 
 export default class Axios{
-  static jsonp(options){
+  static ajax(options){
+    let baseURL = "https://www.fastmock.site/mock/756fffdb95ba2b7df109c2ae429b872b/mockapi"
     return new Promise((resolve, reject) => {
-      JsonP(options.url, {
-        param: "callback"
-      }, function(err, response){
-        if(res){
-          resolve(response)
+      axios({
+        url: options.url,
+        method: "get",
+        baseURL,
+        timeout: 5000,
+        params: (options.data && options.data.params) || ""
+      }).then((response) => {
+        if(response.status === 200){
+          if(response.data.code === 0){
+            resolve(response.data.result.list)
+          }else{
+            Modal.info({
+              title: "提示",
+              content: response.data.desc
+            })
+          }
         }else{
-          reject(response.message)
+          reject(response.data)
         }
       })
     })
--- studyNotes/React全家桶+AntD 共享单车后台管理系统开发/imoocmanager/src/pages/table/basic.js
+++ studyNotes/React全家桶+AntD 共享单车后台管理系统开发/imoocmanager/src/pages/table/basic.js
@@ -1,10 +1,15 @@
 import React from "react"
+import axios from "../../axios"
 import {
   Card, 
   Table
 } from "antd"
 
 class Basic extends React.Component{
+  state = {
+    dataSourceList: []
+  }
+
   render(){
     const columns = [
       {
@@ -50,39 +55,67 @@ class Basic extends React.Component{
     ]
     const dataSource = [
       {
-        id: "0",
-        userName: "Jack",
-        sex: "1",
-        state: "1",
-        interest: "1",
-        birthday: "2000-01-01",
-        address: "北京市海淀区奥林匹克公园",
-        time: "09:00",
+        "id": 1,
+        "userName": "康艳",
+        "sex": 1,
+        "state": 1,
+        "interest": 4,
+        "isMarried": 0,
+        "birthday": "2000-01-01",
+        "address": "北京市海淀区",
+        "time": "09:00:00",
         align: 'center'
       },
       {
-        id: "1",
-        userName: "Tom",
-        sex: "1",
-        state: "1",
-        interest: "1",
-        birthday: "2000-01-01",
-        address: "北京市海淀区奥林匹克公园",
-        time: "09:00",
+        "id": 2,
+        "userName": "何娜",
+        "sex": 2,
+        "state": 4,
+        "interest": 2,
+        "isMarried": 0,
+        "birthday": "2000-01-01",
+        "address": "北京市海淀区",
+        "time": "09:00:00",
         align: 'center'
       },
       {
-        id: "2",
-        userName: "Jone",
-        sex: "1",
-        state: "1",
-        interest: "1",
-        birthday: "2000-01-01",
-        address: "北京市海淀区奥林匹克公园",
-        time: "09:00",
+        "id": 3,
+        "userName": "赵桂英",
+        "sex": 2,
+        "state": 2,
+        "interest": 3,
+        "isMarried": 1,
+        "birthday": "2000-01-01",
+        "address": "北京市海淀区",
+        "time": "09:00:00",
+        align: 'center'
+      },
+      {
+        "id": 4,
+        "userName": "范勇",
+        "sex": 2,
+        "state": 2,
+        "interest": 5,
+        "isMarried": 0,
+        "birthday": "2000-01-01",
+        "address": "北京市海淀区",
+        "time": "09:00:00",
+        align: 'center'
+      },
+      {
+        "id": 5,
+        "userName": "赖平",
+        "sex": 2,
+        "state": 2,
+        "interest": 3,
+        "isMarried": 0,
+        "birthday": "2000-01-01",
+        "address": "北京市海淀区",
+        "time": "09:00:00",
         align: 'center'
       }
     ]
+    const { dataSourceList } = this.state
     return(
       <div>
         <Card title="基础表格">
@@ -94,8 +127,34 @@ class Basic extends React.Component{
             pagination={false}
           />
         </Card>
+        <Card title="动态数据渲染表格">
+          <Table 
+            bordered
+            columns={columns}
+            dataSource={dataSourceList}
+            rowKey={record => record.id}
+            pagination={false}
+          />
+        </Card>
       </div>
     )
+  }
+
+  UNSAFE_componentWillMount(){
+    this.request()
+  }
+
+  // 动态获取 mock 数据
+  request = () => {
+    axios.ajax({
+      url: "/table/list"
+    }).then((res) => {
+      this.setState({
+        dataSourceList: res
+      })
+    }).catch((err) => {
+      console.log(err)
+    })
   }
 }
 
