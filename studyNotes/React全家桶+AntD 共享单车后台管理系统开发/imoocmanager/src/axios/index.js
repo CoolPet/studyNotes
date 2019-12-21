cfa67da5a8fbd4a954f0b67c300e9dc31e7288f7
CoolPet <191261005@qq.com>
2019-12-21 16:29:26 +0800

    React全家桶+AntD 共享单车后台管理系统开发

--- studyNotes/React全家桶+AntD 共享单车后台管理系统开发/imoocmanager/src/axios/index.js
+++ studyNotes/React全家桶+AntD 共享单车后台管理系统开发/imoocmanager/src/axios/index.js
@@ -3,6 +3,11 @@ import { Modal } from "antd"
 
 export default class Axios{
   static ajax(options){
+    let loading
+    if(options.data && options.data.isShowLoading !== false){
+      loading = document.getElementById("ajaxLoading")
+      loading.style.display = "block"
+    }
     let baseURL = "https://www.fastmock.site/mock/756fffdb95ba2b7df109c2ae429b872b/mockapi"
     return new Promise((resolve, reject) => {
       axios({
@@ -15,6 +20,7 @@ export default class Axios{
         if(response.status === 200){
           if(response.data.code === 0){
             resolve(response.data.result.list)
+            loading.style.display = "none"
           }else{
             Modal.info({
               title: "提示",
--- studyNotes/React全家桶+AntD 共享单车后台管理系统开发/imoocmanager/src/pages/table/basic.js
+++ studyNotes/React全家桶+AntD 共享单车后台管理系统开发/imoocmanager/src/pages/table/basic.js
@@ -2,12 +2,18 @@ import React from "react"
 import axios from "../../axios"
 import {
   Card, 
-  Table
+  Table,
+  Modal,
+  Button,
+  message
 } from "antd"
 
 class Basic extends React.Component{
   state = {
-    dataSourceList: []
+    dataSourceList: [],
+    selectedRows: [],
+    selectedId: [],
+    selectedIds: []
   }
 
   render(){
@@ -25,17 +31,34 @@ class Basic extends React.Component{
       {
         title: "性别",
         dataIndex: "sex",
-        align: 'center'
+        align: 'center',
+        render(sex){
+          return sex === 1 ? "男" : "女"
+        }
       },
       {
         title: "状态",
         dataIndex: "state",
-        align: 'center'
+        align: 'center',
+        render(state){
+          return configState[state]
+        }
       },
       {
         title: "爱好",
         dataIndex: "interest",
-        align: 'center'
+        align: 'center',
+        render(interest){
+          return configInterest[interest]
+        }
+      },
+      {
+        title: "婚姻状况",
+        dataIndex: "isMarried",
+        align: 'center',
+        render(isMarried){
+          return isMarried === 1 ? "未婚" : "已婚"
+        }
       },
       {
         title: "生日",
@@ -115,7 +138,43 @@ class Basic extends React.Component{
         align: 'center'
       }
     ]
-    const { dataSourceList } = this.state
+    let configState = {
+      "1": "咸鱼一条",
+      "2": "风华浪子",
+      "3": "北大才子",
+      "4": "百度FE",
+      "5": "创业者"
+    }
+    let configInterest = {
+      "1":"游泳",
+      "2":"打篮球",
+      "3":"踢足球",
+      "4":"跑步",
+      "5":"爬山",
+      "6":"骑行",
+      "7":"桌球",
+      "8":"麦霸"
+    }
+    const { dataSourceList, selectedRowKeys, selectedId } = this.state
+    const rowSelection = {
+      type: "radio",
+      selectedRowKeys
+    }
+    const rowCheckSelection = {
+      type: "checkbox",
+      selectedRowKeys: selectedId,
+      onChange:(selectedId, selectedRows) => {
+        let ids = []
+        selectedRows.map((item) => {
+          return ids.push(item.id)
+        })
+        this.setState({
+          selectedId,
+          selectedRows,
+          selectedIds: ids
+        })
+      }
+    }
     return(
       <div>
         <Card title="基础表格">
@@ -127,13 +186,50 @@ class Basic extends React.Component{
             pagination={false}
           />
         </Card>
-        <Card title="动态数据渲染表格">
+        <Card title="动态数据渲染表格-Mock">
           <Table 
             bordered
             columns={columns}
             dataSource={dataSourceList}
             rowKey={record => record.id}
             pagination={false}
+          />
+        </Card>
+        <Card title="单选表格-Mock">
+          <Table 
+            bordered
+            rowSelection={rowSelection}
+            onRow={(record, index) => {
+              return {
+                onClick: () => {
+                  this.onRowClick(record, index)
+                }
+              }
+            }}
+            columns={columns}
+            dataSource={dataSourceList}
+            rowKey={record => record.id}
+            pagination={false}
+          />
+        </Card>
+        <Card title="多选表格-Mock">
+          <div>
+            <Button onClick={this.handleDelete}>删除</Button>
+          </div>
+          <Table 
+            bordered
+            rowSelection={rowCheckSelection}
+            columns={columns}
+            dataSource={dataSourceList}
+            rowKey={record => record.id}
+            pagination={false}
+            onRow={(record) => {
+              return {
+                onClick: () => {
+                  this.onRowCheckClick(record)
+                }
+              }
+            }}
           />
         </Card>
       </div>
@@ -147,7 +243,10 @@ class Basic extends React.Component{
   // 动态获取 mock 数据
   request = () => {
     axios.ajax({
-      url: "/table/list"
+      url: "/table/list",
+      data:{
+        isShowLoading: true
+      }
     }).then((res) => {
       this.setState({
         dataSourceList: res
@@ -155,6 +254,62 @@ class Basic extends React.Component{
     }).catch((err) => {
       console.log(err)
     })
+  }
+
+  onRowClick(record, index){
+    let selectKey = [index+1]
+    Modal.info({
+      title: "信息",
+      content: `用户名:${record.userName},用户性别:${record.sex===1?"男":"女"}`
+    })
+    this.setState({
+      selectedRowKeys: selectKey,
+      selectedItem: record
+    })
+  }
+
+  onRowCheckClick(record){
+    const { selectedId, selectedRows, selectedIds } = this.state
+    selectedId.push(record.id)
+    selectedRows.push(record)
+    selectedIds.push(record.id)
+    this.setState({
+      selectedId,
+      selectedRows,
+      selectedIds
+    })
+  }
+
+  handleDelete = () => {
+    let list = this.state.dataSourceList
+    let rows = this.state.selectedRows
+    let ids = []
+    if(rows !== []){
+      rows.map((item) => {
+        return ids.push(item.id)
+      })
+      Modal.confirm({
+        title: "删除提示",
+        content: "你确定要删除这些数据吗?",
+        onOk:() => {
+          list = list.filter((item) =>{
+            ids = ids.map(item => item)
+            return !ids.includes(item.id)
+          })
+          message.success("删除成功")
+          this.setState({
+            dataSourceList: list,
+            selectedRows: [],
+            selectedIds: []
+          })
+        }
+      })
+    }else{
+      Modal.info({
+        title: "提示",
+        content: "请先选择你要删除的数据"
+      })
+    }
   }
 }
 
